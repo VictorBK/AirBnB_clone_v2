8927ffe27d8f0887e235115e0907775b36ccd14d
VictorBK <victorbrian4@gmail.com>
2022-09-30 01:30:37 +0300

    Puppet for setup v2

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,28 @@
-# setup web server for deployment
-
-exec { 'server setup':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
-  provider => shell,
+# puppet manifest preparing a server for static content deployment
+exec { 'sudo apt-get-update':
+  command => '/usr/bin/env apt-get -y update',
+}
+-> exec {'b':
+  command => '/usr/bin/env apt-get -y install nginx',
+}
+-> exec {'c':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
+}
+-> exec {'d':
+  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
+}
+-> exec {'e':
+  command => '/usr/bin/env echo "Puppet x Holberton School" > /data/web_static/releases/test/index.html',
+}
+-> exec {'f':
+  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
+}
+-> exec {'h':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+}
+-> exec {'i':
+  command => '/usr/bin/env service nginx restart',
+}
+-> exec {'g':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 }
